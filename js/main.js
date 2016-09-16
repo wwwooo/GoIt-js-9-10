e8f28892cff87c718130560ead4d8c035793d2a8
wwwooo <malinovsvska@gmail.com>
2016-09-16 08:18:20 +0300

    added checkbox

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+var changeCheck = function(span) {
+    var ch = span.find('input');
+    if (ch.attr('class').indexOf('niceCheckDisabled') === -1) {
+        if (!ch.attr('checked')) {
+            span.addClass('niceChecked');
+            ch.attr('checked', true);
+        } else {
+            span.removeClass('niceChecked');
+            ch.attr('checked', false).focus();
+        }
+    }
+};
+
+var changeVisualCheck = function(ch) {
+    if (!ch.attr('checked')) {
+        ch.parent().addClass('niceChecked');
+    } else {
+        ch.parent().removeClass('niceChecked');
+    }
+};
+
+var changeCheckStart = function(ch) {
+    ch.wrap('<span class="niceCheck"></span>');
+    var checkChecked = ch.attr('checked');
+    var checkDisabled = ch.attr('disabled');
+
+    if (checkChecked) {
+        ch.parent().addClass('niceChecked');
+    }
+
+    if (checkDisabled) {
+        ch.parent().addClass('niceCheckDisabled');
+    }
+
+    ch.parent().on('click', function() {
+        changeCheck($(this));
+    });
+
+    ch.on('change', function() {
+        changeVisualCheck($(this));
+    });
+};
+
 $(function() {
-    //duration - продолжительность
+    $('select').selectric();
+
+    $('.niceCheck').each(function() {
+        changeCheckStart($(this));
+    });
 });
